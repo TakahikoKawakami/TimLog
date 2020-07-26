7c101d89152ad78ae5c796486326c8178ec62d4f
TakahikoKawakami <takahiko.kawakami1128@gmail.com>
2020-07-26 04:32:07 +0000

    for gitpod

--- .gitpod-init.sh
+++ .gitpod-init.sh
@@ -2,6 +2,7 @@ mysql -u root -e "create database laravel"
 cp src/.env.example src/.env
 sed -i "s|APP_URL=|APP_URL=${GITPOD_WORKSPACE_URL}|g" src/.env
 sed -i "s|APP_URL=https://|APP_URL=https://8000-|g" src/.env
+cd src
 composer install
 npm install
 php artisan key:generate
