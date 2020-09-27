baeeea86aa1f9aeb5ac715560f3747775864cda7
TakahikoKawakami <takahiko.kawakami1128@gmail.com>
2020-09-27 22:55:38 +0900

    prod

--- src/resources/js/store.js
+++ src/resources/js/store.js
@@ -8,6 +8,6 @@ export default {
         // url: 'https://8000-ac177d7b-ac03-4eab-bed4-3d99c52b03c3.ws-us02.gitpod.io/'
         // url: 'http://192.168.0.7:800/',
         // url: 'localhost:800/',
-        url: 'timlog.from-garage.work/'
+        url: 'https://timlog.from-garage.work/'
     }
 }
